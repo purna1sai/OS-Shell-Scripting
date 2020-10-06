45ec9fed9104d1380012a09e8d18f549721617a0
Jaideep Cherukuri <43186691+JaideepCherukuri@users.noreply.github.com>
2020-10-07 00:31:53 +0530

    Update spell.sh

--- spell.sh
+++ spell.sh
@@ -1,19 +1,4 @@
 #!/bin/bash
 file="$1"
 SPELL="/usr/bin/ispell"
-if [ $# -eq 0 ]
-then
-	echo "Syntax: $(basename $0) file"
-	exit 1
-fi
-if [ ! -f $file ]
-then
-	echo "Error - $file not a file!"
-	exit 2
-fi
-if [ ! -x $SPELL ]
-then
-	echo "Error - $SPELL binary file does NOT exists; cannot check the spellings of $file."
-	exit 3
-fi
 $SPELL $file
