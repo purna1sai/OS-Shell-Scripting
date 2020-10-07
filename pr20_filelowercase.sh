513c934bb885d9effca4fe3bb0c3d05cd216a80f
Purna Sai Madala <purna_sai@srmap.edu.in>
2020-10-07 09:16:48 +0530

    Update and rename filelowercase.sh to pr20_filelowercase.sh

--- pr20_filelowercase.sh
+++ pr20_filelowercase.sh
@@ -5,20 +5,15 @@ then
 	echo "$(basename $0) file"
 	exit 1
 fi
- 
 if [ ! $file ]
 then
 	echo "$file not a file"
 	exit 2
 fi
- 
 lowercase=$(echo $file | tr '[A-Z]' '[a-z]'])
- 
 if [ -f $lowercase ]
 then
 	echo "Error - File already exists!"
 	exit 3
 fi
- 
-# change file name
 /bin/mv $file $lowercase
